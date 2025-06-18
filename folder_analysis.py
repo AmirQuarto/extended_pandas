6ca64ec5163561d0de48634aef90eff8f37983cc
AmirQuarto <amir.meshkati.blog@gmail.com>
2025-06-18 23:49:36 +0200

    Update folder_analysis.py

--- folder_analysis.py
+++ folder_analysis.py
@@ -1,4 +1,6 @@
-def read_text_file(path):
+import os
+
+def read_file(path):
     """Reads the contents of a UTF-8 text file and returns it as a string."""
     with open(path, "r", encoding="utf-8") as f:
         file_text = f.read()
@@ -6,10 +8,14 @@ def read_text_file(path):
     return file_text
 
 
-def write_text_file(path, content):
+def write_file(path, content):
     """Writes the given string content to a UTF-8 text file."""
     with open(path, "w", encoding="utf-8") as f:
         f.write(content)
+
+
+def create_folder(path):
+    os.makedirs(path, exist_ok=True)
 
 
 def yeald_list(lst, print_output=True):
